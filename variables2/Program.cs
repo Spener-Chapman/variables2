cf0a3a4ac9e6201807425c6f066ea5de51c6f6d2
Spener-Chapman <spencerchap@hotmail.com>
2021-09-20 13:39:53 -0300

    Add floats
    adds floats, and the difference between floats and integers

--- variables2/Program.cs
+++ variables2/Program.cs
@@ -49,10 +49,44 @@ namespace variables2
 
             firstname = "spencer";
             lastname = "chapman";
-            fullname = firstname + " " + lastname; // the " " i used ofr a space
+            fullname = firstname + " " + lastname; // the " " is used ofr a space
             Console.WriteLine(firstname);
             Console.WriteLine(lastname);
             Console.WriteLine(fullname);
+
+            // -----FLOATS-----
+
+            Console.WriteLine("");
+            Console.WriteLine("Floats");
+            Console.WriteLine("");
+            // "floating point" stores real numbers as oppose to interge numbers
+            // 1000000.000
+            // 100000.0000 <--- examples of floating point
+            // 10000.00000
+
+            // floats are good for moving
+
+            float speed; // in kph
+            float distanceTravelled; // in km
+
+            speed = 100.0f; // in kmh
+            distanceTravelled = 2345.35f; // in km
+
+            Console.WriteLine("Speedometer: " + speed);
+            Console.WriteLine("odometer: " + distanceTravelled);
+
+            // ints vs floats
+
+            int intTest;
+            float floatTest;
+            float floatTest2;
+
+            intTest = 10 / 3;
+            floatTest = 10 / 3; // should be 3.3333 but computer sees it as interger, stores as float
+            floatTest2 = 10.0f / 3.0f; // the .0f is what makes it know its a float
+            Console.WriteLine(intTest);
+            Console.WriteLine(floatTest);
+            Console.WriteLine(floatTest2);
 
             Console.ReadKey(true);
         }
